732d31ef01cfb37a4dba494960bca49cb7dde236
Andrew Mueller <amuell11@vols.utk.edu>
2023-04-02 17:45:50 -0400

    updated model code

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
     parser.add_argument("--save_model", default=False, type=bool, help="Number of particles")
     parser.add_argument("--save_file", default="model.pt", type=str, help="Model file name")
     parser.add_argument("--load_model", default=False, type=bool, help="Load a model")
-    parser.add_argument("--load_file", default=True, type=int, help="File to load")
+    parser.add_argument("--load_file", default="", type=str, help="File to load")
     parser.add_argument("--sample_rate", default=8000, type=int, help="Sample rate for transformation")
     parser.add_argument("--batch_size", default=256, type=int, help="Batch size for model training")
     parser.add_argument("--test_model", default=False, type=bool, help="Test the model")
@@ -58,10 +58,11 @@ if __name__ == "__main__":
     load_model_file  = args.load_file
     save_model_file  = args.save_file
     save_model       = args.save_model
-    load_model       = args.load_file
+    load_model       = args.load_model
     create_model     = args.create_model
     test_model       = args.test_model
-    run_full_test   = args.run_full_test
+    run_full_test    = args.run_full_test
+    train_model      = False
 
     device = get_device_type()
     num_workers, pin_memory = get_device_info(device)
@@ -82,7 +83,7 @@ if __name__ == "__main__":
 
     #get model --> if statement for either loading in, or creating a new one!
     model = audioModel.M5()
-
+    print(load_model)
     if(load_model == True):
         if(os.path.isfile(load_model_file) == False):
             print("\'%s\' does not exist, exiting" % load_model_file) #Force to rerun
@@ -91,20 +92,19 @@ if __name__ == "__main__":
             model.load_state_dict(torch.load("test.ptf"))
             model.eval()
             model.to(device)
+            train_model = False
     else:
         print("Creating new model")
         model.to(device)
-        create_model = True
+        train_model = True
         test_model = False
     
-    
-
     optimizer = audioModel.setOptimizer(model, learn_rate = 0.01, weight_decay=0.0001)
     # reduce the learning after 20 epochs by a factor of 10
     scheduler = audioModel.setScheduler(optimizer, step_size=20, gamma=0.1)
 
     pbar_update = 1 / (len(train_loader) + len(test_loader))
-    if(create_model == True):
+    if(train_model == True):
         #training and testing model!
         with tqdm(total=n_epoch) as pbar:
             for epoch in range(1, n_epoch + 1):
--- pertubation.py
+++ pertubation.py
@@ -1,5 +1,7 @@
 
 
+#file where the attack code can go...
+
 # stop, go
 # on, off
 # yes, no
